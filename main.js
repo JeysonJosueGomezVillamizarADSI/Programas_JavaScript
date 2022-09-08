d947a88e381783246a28431f3be10b222d9cde3f
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-08 12:21:23 -0500

    Programa 5

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-let base = Number(prompt("Ingrese la base del triangulo : "));
-let altura = Number(prompt("Ingrese la altura del triangulo : "))
-let area 
-area = (base*altura) / 2
+const pi = 3.14;
 
-alert("El area del triangulo es : " + area)
+let radio = Number (prompt("Ingrese el radio del circulo"));
+
+let long = 2*pi*radio;
+
+let area = pi*radio*2;
+
+alert ("La longitud de la circuferencia es " + long);
+alert ("El area de la circuferencia es " + area);
