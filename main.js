f16a3d90f70f2f4055cb88eb7233379003dfc77c
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-08 12:13:41 -0500

    Programa 4

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-let num1 = parseInt(prompt("Ingrese el primer numero"));
-let num2 = parseInt(prompt("Ingrese el segundo numero"));
-let num3 = parseInt(prompt("Ingrese el tercer numero"));
-let num4 = parseInt(prompt("Ingrese el cuarto numero"));
+let base = Number(prompt("Ingrese la base del triangulo : "));
+let altura = Number(prompt("Ingrese la altura del triangulo : "))
+let area 
+area = (base*altura) / 2
 
-let media = (num1+num2+num3+num4)/4;
-
-alert("La media de los cuatro numeros es " + media);
+alert("El area del triangulo es : " + area)
