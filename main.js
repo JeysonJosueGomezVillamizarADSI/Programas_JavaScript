1dc7a09e96ae6189dab6dbd96a6e0ee1ef217250
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-08 21:25:04 -0500

    Programa 9

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-let a = Number(prompt("Digite el valor de a"));
-let b = Number(prompt("Digite el valor de b"));
-let c = Number(prompt("Digite el valor de c"));
+const pi = 3.14;
+let radio = Number(prompt("Digite el radio del cilindro "));
+let altura = Number(prompt("Digite la altura del cilindro "));
 
-let resultado = (a+7*c)/(b+2-a)+2*b ;
+let area = (2 * (pi * rad**2)) + ((2 * pi * rad) * alt);
+let volumen = (pi * rad**2) * alt;
 
-alert("El resultado de la expresion es  " + resultado);
+alert("El area del cilindro es " + area);
+alert("El volumen del cilindro es " + volumen);
