db2bef29fa10a420db4e9a013c6f6529af9c39d6
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-12 18:46:33 -0500

    Programa 17

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-let parcial1= Number (prompt("Digite la calificacion del primer parcial : "));
+let precioProducto = Number (prompt("Digite el precio del prodcuto : "));
+let Iva = Number (prompt("Digite el Iva sin % : "));
 
-let parcial2= Number (prompt("Digite la calificacion del segundo parcial : "));
+let TotIva = Iva/100;
 
-let parcial3= Number (prompt("Digite la calificacion del tercer parcial : "));
+let IvaProducto = precioProducto* TotIva;
 
-let examenFinal= Number (prompt("Digite la calificacion del examen final : "));
+let pagoTotal = precioProducto + IvaProducto;
 
-let trabajoFinal= Number (prompt("Digite la calificacion del trabajo final  : "));
-
-let resultParciales = ((parcial1+parcial2+parcial3)/3)*0.55;
-let resultExamenF = examenFinal*0.30;
-let resultTrabajoF = trabajoFinal*0.15;
-
-let calificacionFinal= (resultParciales+resultExamenF+resultTrabajoF);
-
-alert("La calficacion del estudiante es : " + calificacionFinal);
+alert ("El iva del producto es de : " + IvaProducto);
+alert ("El total a pagar del producto es de : " + pagoTotal);
 
