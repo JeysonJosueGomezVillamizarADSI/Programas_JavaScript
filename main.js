4cd62065aae4a0bc0a563eba93bc8c870a89be8c
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-12 18:40:26 -0500

    Programa 16

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-let gradoCentigrado = Number (prompt("Ingrese los grados centigrados "));
+let parcial1= Number (prompt("Digite la calificacion del primer parcial : "));
 
-let gradoFahrenheit = (gradoCentigrado* (9/5))+32;
+let parcial2= Number (prompt("Digite la calificacion del segundo parcial : "));
 
-alert ("Los grados  Fahrenheit son : " + gradoFahrenheit);
+let parcial3= Number (prompt("Digite la calificacion del tercer parcial : "));
+
+let examenFinal= Number (prompt("Digite la calificacion del examen final : "));
+
+let trabajoFinal= Number (prompt("Digite la calificacion del trabajo final  : "));
+
+let resultParciales = ((parcial1+parcial2+parcial3)/3)*0.55;
+let resultExamenF = examenFinal*0.30;
+let resultTrabajoF = trabajoFinal*0.15;
+
+let calificacionFinal= (resultParciales+resultExamenF+resultTrabajoF);
+
+alert("La calficacion del estudiante es : " + calificacionFinal);
 
